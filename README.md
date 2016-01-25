56d4a4bb48ac79a00f4d31e660e0285f73f15867
Clayton Burlison <clburlison@gmail.com>
2016-01-25 10:29:59 -0600

    First real commit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# imagr.dev
-An all-in-one guide for using Imagr locally
+imagr.dev
+===
+
+I don't use docker enough to remember all the steps. The result is this lazy wrapper script written in bash so all I have to remember is `./wrapper.sh`.
+
+I owe you better docs but this will do for now....based off Graham's [MacTech 2015 Imagr lab](http://grahamgilbert.com/blog/2015/11/12/mactech-2015-hands-on-with-imagr/)
+
+# Software
+* [DockerToolbox](https://github.com/docker/toolbox/releases/)
+* [AutoDMG](https://github.com/MagerValp/AutoDMG/releases/)
+* [NBICreator](https://github.com/NBICreator/NBICreator/releases/)
+* [VMWare Fusion v7 or v8](https://www.vmware.com/products/fusion)
+
+
+# Process
+
+Download and install the stuff above. 
+
+Git Clone this repo to some directory under /Users/$username. (This is a limitation to docker's VMWare Fusion driver ATM.)
+
+  ```bash
+  $ git clone https://github.com/clburlison/imagr.dev
+  $ cd imagr.dev
+  ```
+
+1. Create an image using AutoDMG. Place created dmg inside of `web_root`
+1. Create Imagr NBI using NBICrator. Place created nbi inside of `web_root`
+1. Copy files from `sample` directory to `web_root` 
+1. Follow the yellow brick road below
+
+**Note:** the `adminUser.pkg` username and password are both "admin" 
+
+1. Start our docker machine 
+
+  ```bash
+  $ ./wrapper.sh create
+  ```
+
+Keep track of your Virtual Machine's IP address. You'll need to modify your `imagr_config.plist` to match this IP.
+
+1. Start our docker containers
+
+  ```bash
+  $ ./wrapper.sh start
+  ```
+
+#Stop
+
+You will want to stop your docker virtual machine when not using it.
+
+  ```bash
+  $ ./wrapper.sh stop
+  ```
+
+#Remove
+And when you are permanently done and want to save disk space.
+
+  ```bash
+  $ ./wrapper.sh remove
+  ```
+
+
+
+# Help NetBoot isn't working
+Check your bsdpy logs
+
+  ```bash
+  $ ./wrapper.sh logs
+  ```
+
+
+
+
+# Resources
+http://www.container42.com/2015/10/30/docker-networking-reborn/,  
+http://grahamgilbert.com/blog/2015/11/12/mactech-2015-hands-on-with-imagr/  
---
+++ wrapper.sh
@@ -0,0 +1,187 @@
+#!/bin/sh 
+
+set -e
+
+DRIVER="vmwarefusion"
+VMNAME="imagr"
+VMDISKSIZE="500000"
+VMMEM="2048"
+VMCPU="2"
+
+#################################################################################
+# End user of variables - Functions be below
+#################################################################################
+
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+# I literally can't even...
+# So here is a hacky way to get around environment variables
+ldock() {
+  eval "$(docker-machine env ${VMNAME})"
+  eval DATA_DIR=`pwd`/web_root
+  eval IP=$(docker-machine ip ${VMNAME})
+  export DATA_DIR=`pwd`/web_root
+  export IP=$(docker-machine ip ${VMNAME})
+}
+
+msg() {
+  printf "${CYAN}$1 \n${NC}"
+}
+
+error() {
+  printf "${RED}$1 \n${NC}"
+}
+
+check() {
+  CHECKVAR=$(docker-machine ls | sed "s/\ \{2,\}/$(printf '\t')/g" | awk -F"\t" '/imagr/{print $4}')
+
+  if [[ ${CHECKVAR} == 'Stopped' ]]; then
+    msg "Starting your Docker VM"
+    docker-machine start ${VMNAME}
+  elif [[ ${CHECKVAR} == 'Running' ]]; then
+    msg "Your docker VM is running."
+  else
+    msg "Your Docker VM doesn't appear to be created."
+  fi    
+}
+
+create() {
+  msg "Creating Docker Virtual Machine "
+  if [[ ${DRIVER} == 'vmwarefusion' ]]; then
+    msg "${DRIVER} has been selected as driver"
+    docker-machine create --vmwarefusion-memory-size=${VMMEM} \
+    --vmwarefusion-cpu-count=${VMCPU} \
+    --vmwarefusion-disk-size=${VMDISKSIZE} \
+    -d vmwarefusion ${VMNAME}
+  elif [[ ${DRIVER} == 'virtualbox' ]]; then
+    msg "${DRIVER} has been selected as driver"
+    docker-machine create ----virtualbox-memory=${VMMEM} \
+    --virtualbox-cpu-count=${VMCPU} \
+    --virtualbox-disk-size=${VMDISKSIZE} \
+    -d virtualbox ${VMNAME}
+  else 
+    msg "Invalid VM driver. Please use 'vmwarefusion' or 'virtualbox' "  
+  fi
+  
+  # store the IP address of the docker virtual machine
+  IP=$(docker-machine ip ${VMNAME})
+  msg "***YOUR DOCKER IP IS "$IP" ***"
+
+  wait
+}
+
+data() {
+  DATA_DIR=`pwd`/web_root
+
+  # see if we have a directory to store docker containers
+  if [[ ! -e "$DATA_DIR" ]] ; then
+      printf "${RED}'web_root' directory must be created manually "
+      msg "Please run: "
+      msg  "   mkdir `pwd`/docker "
+      exit 1
+  fi
+}
+
+pull() {
+  msg "Pull docker container(s) "
+  check
+  ldock
+  
+  docker-compose pull
+  wait
+}
+
+start() {
+  msg "Start docker containers "
+  check
+  ldock
+  data
+  msg "${IP}"
+  msg "${DATA_DIR}"
+  
+  docker-compose up -d
+  wait
+}
+
+restart() {
+  msg "Restart docker containers "
+  check
+  ldock
+  data
+  msg "${IP}"
+  msg "${DATA_DIR}"
+  
+  docker-compose stop
+  docker-compose rm
+  docker-compose up -d
+  wait
+}
+
+stop() {
+  # check
+  ldock
+  msg "Do you want to stop your Docker VM, '${VMNAME}'? [yN]  "
+  read ANSWER
+  if [[ ${ANSWER} == 'y' ]]; then
+    msg "Stopping your Docker VM"
+    docker-machine stop ${VMNAME}
+  else
+    msg "Your Docker VM is still running."
+  fi
+  wait
+}
+
+remove() {
+  msg "Removing docker-machine '${VMNAME}' "
+  docker-machine rm ${VMNAME}
+
+  wait
+}
+
+logs() {
+  check
+  ldock
+  msg "Checking bsdpy logs "
+  CONTAINER_NAME=$(docker ps | grep "bsdpy" | sed "s/\ \{2,\}/$(printf '\t')/g" | awk -F"\t" '/imagr/{print $7}')
+  docker logs $CONTAINER_NAME
+  
+  wait
+}
+
+case $1 in
+  create)
+    create
+    ;;
+  pull)
+    pull
+    ;;
+  start)
+    start
+    ;;
+  restart)
+    restart
+    ;;
+  stop)
+    stop
+    ;;
+  remove)
+    remove
+    ;;
+  logs)
+    logs
+    ;;  
+  *)
+    error "Unsupported argument!"
+    msg "Supported arguments:"
+    msg "  create - create your docker VM"
+    msg "  pull - pull required containers"
+    msg "  start - start docker containers from docker-compose.yml"
+    msg "  restart - restart docker containers from scratch"
+    msg "  stop - stop docker-machine vm"
+    msg "  remove - remove docker-machine vm"
+    msg "  logs - check bsdpy logs"
+    exit 1
+    ;;
+esac
